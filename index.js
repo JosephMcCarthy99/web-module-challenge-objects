56c93e9a1f9ffd516d526908c0e6d8c81023e6af
Joe Bear <j.mccarthy0990@gmail.com>
2021-11-04 17:28:56 -0400

    task 1b done

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
-
+console.log('task 1b', createMenuItem('pizza', 10, 'Lunch'));
+console.log('task 1b', createMenuItem('cheeseburger', 8, 'Dinner'));
+console.log('task 1b', createMenuItem('bacon', 5, 'Breakfast'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
